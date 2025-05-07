65c0890a83781a08b848e7e36cd17380d0d8a77a
Lahiru Wijesinghe <wml.eranga@gmail.com>
2025-05-07 17:33:42 +0530

    Update cv

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -15,5 +15,5 @@ export const personalData = {
   hackerrank: "https://www.hackerrank.com/profile/wml_eranga",
   devUsername: "wmlEranga",
   resume:
-    "https://drive.google.com/file/d/1f3s2wxG5DlRibtdzM-3euKpSFTNID3tz/view?usp=sharing",
+    "https://drive.google.com/file/d/13z8K70MoPypby2XVEM9YTbpqHGTOUEYN/view?usp=sharing",
 };
